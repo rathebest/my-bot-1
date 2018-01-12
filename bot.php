2b1b59e0521cbc1afeae0f86c64bceaf294a29a5
rathebest <35357347+rathebest@users.noreply.github.com>
2018-01-12 11:45:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,10 +17,21 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			
+			if($event['message']['type'] == 'ชื่ออะไร'){
+				$messages = [
+					'type' => 'text',
+					'text' => 'วีรชิต ศรีมุข'
+					];
+			}
+			else{
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
+			}
+			
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
