86b2594599515e4e74fe8e43d9b953c8996ac1a7
rathebest <35357347+rathebest@users.noreply.github.com>
2018-01-12 10:56:19 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '1556860753';
+$access_token = 'fiAMHL0DOTlugChoT/1bTrhvrYT0YYAzdQlfgrlGXSA9ikf22lSN8xbYz7cKOSUpHgcedlVG4X/q4ErlYb1aLhu4oNeyy5Pyj4JHK/uX63abpbebuQrLqKWX3IV+xUKlN0JU6IfTY2bx1q00y6lg6QdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
